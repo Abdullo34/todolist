cae0f51985f2bac478ed618093d7005ad26d0c76
abdullo <saydullayev297@gmail.com>
2024-03-04 21:51:10 +0500

    added forEach methods

--- js/app.js
+++ js/app.js
@@ -20,12 +20,77 @@ let todos = JSON.parse(localStorage.getItem('list')) ?
 
 console.log(todos)
 
+if (todos.length) showTodos()
+
+
+
+//  set todos
+function setTodos() {
+    localStorage.setItem('list', JSON.stringify(todos))
+}
+
+
+function showTodos() {
+    const todos = JSON.parse(localStorage.getItem('list'))
+    listGroupTodo.innerHTML = ''
+
+    todos.forEach((item, i) => {
+        listGroupTodo.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between">
+         ${item.text}
+         <div class="todo-icons">
+           <span class="opacity-50 me-2">${item.time}</span>
+           <img src="./img/edit.svg" alt="edit icon" width="25" height="25">
+           <img src="./img/delete.svg" alt="edit icon" width="25" height="25">
+         </div>
+       </li> 
+    `
+    });
+
+
+}
+
+
+
+
+
+// show error
+function showMessage(where, message) {
+    document.getElementById(`${where}`).textContent = message
+
+    setTimeout(() => {
+        document.getElementById(`${where}`).textContent = ''
+    }, 2500)
+}
+
+
+
+
+// show todos
+
+// time todos 
+
+// function timeTodos() {
+//     let date = new Date()
+//     let time = `${date.getDate()}.${date.getMonth() < 10 ? date.getMonth() + 1 : "0" + date.getMonth()}.${date.getFullYear()}`
+//     return time
+// }
+
 
 //  get todos
 
 formCreate.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const todoText = formCreate['input-create']
-    console.log(todoText)
+    const todoText = formCreate['input-create'].value.trim()
+    formCreate.reset()
+    if (todoText.length) {
+        // const time = timeTodos()
+        todos.push({ text: todoText, time: '13.02.2020', completed: false })
+        setTodos()
+        showTodos()
+    } else {
+        showMessage('message-create', 'Please, enter some text...')
+    }
 })
+
